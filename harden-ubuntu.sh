8a031649f54ef7983544b0f22652b520b5330b0b
ashoetan <alfredshoetan@gmail.com>
2023-07-13 14:42:45 -0400

    changes to script

--- harden-ubuntu.sh
+++ harden-ubuntu.sh
@@ -1,3 +1,5 @@
+root@vmi1362517:~/hardenscript# vi harden-ubuntu.sh 
+root@vmi1362517:~/hardenscript# vi harden-ubuntu.sh 
 #!/bin/bash
 
 # patch server
@@ -6,32 +8,36 @@ sudo apt update && apt upgrade -y
 # install UFW (Uncomplicated Firewall)
 sudo apt install ufw -y
 
-# enable ufw
-ufw_status=$(sudo ufw status | awk '/Status:/ {print $2}')
 
-if ["$ufw_status" == "inactive"]; then
-   sudo ufw enable
-   echo "UFW has been activated"
-else
-   echo "UFW already activated"
-fi
+# enable ufw
+sudo ufw enable
 
 # fw deny all incoming ipv4 & ipv6 traffic 
-sudo ufw default deny in
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+
+# disable ipv6
+sudo sed -i 's/IPV6=yes/IPV6=no/' /etc/default/ufw
 
 # allow tcp ports 
 sudo ufw allow 22
 
 # allow specific ssh port
-if [$# -ne 0 ] then
-   sudo ufw allow $1
+if [ "$#" -ne 0 ]; then
+   sudo ufw allow "$1"
 else
    echo "NO additional Port"
 fi
+
+# enable logging of ufw @ /var/log/ufw.log file
+sudo ufw logging on
 
 # Next Steps
 echo "now list packages with => dpkg --list"
 echo "then get pkg info =>  dpkg --info packageName"
 echo "finally remove all unneeded packages => sudo dpkg --remove package_name
  "
+
+                                                           37,0-1        Bot
+
 
