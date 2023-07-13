53c69c7a534790b1571da1c66ab68eecef39d915
ashoetan <alfredshoetan@gmail.com>
2023-07-13 19:56:44 -0400

    harden + endless

--- harden-ubuntu.sh
+++ harden-ubuntu.sh
@@ -1,18 +1,7 @@
-root@vmi1362517:~/hardenscript# tail /etc/ssh/sshd_config
-Subsystem	sftp	/usr/lib/openssh/sftp-server
-
-# Example of overriding settings on a per-user basis
-#Match User anoncvs
-#	X11Forwarding no
-#	AllowTcpForwarding no
-#	PermitTTY no
-#	ForceCommand cvs server
-PermitRootLogin yes
-Port 2222
-root@vmi1362517:~/hardenscript# vi harden-ubuntu.sh 
-root@vmi1362517:~/hardenscript# vi harden-ubuntu.sh 
-root@vmi1362517:~/hardenscript# vi harden-ubuntu.sh 
 #!/bin/bash
+  
+# delete old endlessssh
+rm -rf  ~/endless
 
 # patch server
 sudo apt update && apt upgrade -y
@@ -37,6 +26,7 @@ sudo ufw allow 22
 if [ "$#" -ne 0 ]; then
    sudo ufw allow "$1"
 else
+else
    echo "NO additional Port"
 fi
 
@@ -46,9 +36,20 @@ sudo ufw logging on
 #change default ssh port 
 if [ "$#" -ne 0 ]; then
    echo "Port $1" >> /etc/ssh/sshd_config
-   sudo apt install openssh-server -y
-   sudo systemctl enable openssh
-   sudo systemctl start openssh
+   # install endless ssh
+   sudo apt install libc6-dev build-essential -y
+   git clone https://github.com/skeeto/endlessh ~/endless
+   cd ~/endless
+   sudo make
+   sudo cp endlessh /usr/local/bin/
+   sed -i 's/#AmbientCapabilities=CAP_NET_BIND_SERVICE/AmbientCapabilities=CAP_NET_BIND_SERVICE /' util/endlessh.service
+   sed -i 's/PrivateUsers=true/#PrivateUsers=true/' util/endlessh.service
+   sudo cp util/endlessh.service /etc/systemd/system/
+   setcap 'cap_net_bind_service=+ep' /usr/local/bin/endlessh
+   sudo systemctl enable endlessh
+   sudo mkdir -p /etc/endlessh
+   echo "Port 22" > /etc/endlessh/config
+   sudo systemctl start endlessh
    sudo systemctl restart ssh
 else
    echo "SSH on Port 22"
@@ -56,5 +57,3 @@ fi
 
 # Disable root login
 #sudo sed -i 's/PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
-
-
